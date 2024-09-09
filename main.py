739e9aaae98e2b9b15a003274aec9bb46b0c429e
DahyeNinoHan <120151205+DahyeNinoHan@users.noreply.github.com>
2024-09-09 00:52:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_openai import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
+import streamlit as st
+import tempfile
+import os
+
+
+#Streamlit - title
+st.title("ChatPDF")
+st.write("---")
+
+#Streamlit - file upload 
+uploaded_file = st.file_uploader("Choose a file")
+st.write("---")
+
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
+
+# After file uploaded
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file) 
+
+    #Split
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size=500,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    texts = text_splitter.split_documents(pages)
+
+    #Embedding
+    embeddings_model = OpenAIEmbeddings()
+
+    #VectorDB
+    db = Chroma.from_documents(texts, embeddings_model)
+
+
+    #Question
+    st.header('Ask me about PDF!')
+    question = st.text_input('Question: ')
+    
+    if st.button('Submit'):
+        with st.spinner('Wait for it...'):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+            result = qa_chain({"query": question})
+            st.write(result["result"])
